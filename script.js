2b3a27a7542892a4bcb2873e643db4749086f083
outtaheresoon <226462141+outtaheresoon@users.noreply.github.com>
2026-05-20 03:10:52 +0530

    Updated project UI + fixes
    git push origin maingit push origin maingit push origin mainfont-family: Montserrat;#	modified:   style.css
    git push origin main#

--- script.js
+++ script.js
@@ -39,3 +39,103 @@ gsap.fromTo(
     yoyo: true,
   },
 );
+
+
+/* images*/
+
+window.addEventListener("DOMContentLoaded", () => {
+  const track = document.querySelector(".project-images");
+
+  const distance = track.scrollWidth / 2;
+
+  gsap.to(track, {
+    x: -distance,
+    duration: 4,
+    ease: "none",
+    repeat: -1,
+  });
+});
+
+/*  MORE OPTIONS */
+
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".archive-items button");
+  const video = document.getElementById("mainVideo");
+
+  const titleEl = document.querySelector(".context-section h2");
+  const textEl = document.querySelector(".context-section p");
+
+  const defaultVideo = "assets/videos/for-the-web-development.mp4";
+
+  let activeBtn = null;
+  let isPreviewMode = false;
+
+  function loadVideo(src, forceRestart = false) {
+    video.pause();
+
+    if (forceRestart) {
+      video.currentTime = 0; //
+    }
+
+    const source = video.querySelector("source");
+
+    if (source) {
+      source.src = src;
+    } else {
+      video.src = src;
+    }
+
+    video.load();
+
+    video.play().catch(() => {});
+  }
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const src = btn.dataset.video;
+      const title = btn.dataset.title;
+      const text = btn.dataset.text;
+
+      //
+      titleEl.textContent = title || "";
+      textEl.textContent = text || "";
+
+      const isVideoButton = src && src.trim() !== "";
+
+      //  VIDEO BUTTONS (01 / 02 / 03 included)
+      if (isVideoButton) {
+        if (activeBtn === btn) {
+          isPreviewMode = !isPreviewMode;
+        } else {
+          activeBtn = btn;
+          isPreviewMode = true;
+        }
+
+        if (isPreviewMode) {
+          video.style.opacity = "1";
+          video.style.filter = "none";
+
+          const isSameVideo = video.src.includes(src);
+
+          loadVideo(src, isSameVideo);
+        } else {
+          isPreviewMode = false;
+          activeBtn = null;
+
+          video.style.opacity = "1";
+          loadVideo(defaultVideo);
+        }
+
+        return;
+      }
+
+      //  SAFETY FALLBACK
+      activeBtn = null;
+      isPreviewMode = false;
+
+      video.style.opacity = "1";
+      video.style.filter = "none";
+    });
+  });
+});
+
